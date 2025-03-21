39a41a228c5f015cc09256712a1ec702ef1842ea
trngnneee <dtn06052005@gmail.com>
2025-03-21 20:23:16 +0700

    Added Nodemon, connect with PUG and create simple folder format

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 3000
 
+// Thiết lập views
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Trang chủ')
+  res.render("client/pages/home.pug", {
+    pageTitle: "Trang chủ"
+  })
 })
 
 app.get('/tour', (req, res) => {
-  res.send('Danh sách tour')
+  res.render("client/pages/tour-list.pug", {
+    pageTitle: "Danh sách tour"
+  })
 })
 
 app.listen(port, () => {
