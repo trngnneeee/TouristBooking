e1fbe6994ec1545bca53564f731f5cc2f933c98a
trngnneee <dtn06052005@gmail.com>
2025-03-24 20:10:08 +0700

    Thêm dotevn lib để tạo ra biến môi trường lưu trữ đường link kết nối tới MongoDB để tránh bảo mật, file .env được bỏ vào gitignore

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path');
 // Thiết lập Mongoose + Kết nối với CSDL Online
+// Thiết lập dotevn
+require('dotenv').config()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://trngnneee:trngnneee123@admin.syjye.mongodb.net/TourManagement');
+mongoose.connect(process.env.DATABASE);
 
 const Tour = mongoose.model('Tour', { 
   name: String,
@@ -27,7 +29,6 @@ app.get('/', (req, res) => {
 
 app.get('/tour', async (req, res) => {
   const tourList = await Tour.find({});
-  console.log(tourList);
   
   res.render("client/pages/tour-list.pug", {
     pageTitle: "Danh sách tour",
