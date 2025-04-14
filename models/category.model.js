6b07b9119e4e365ee569e031b64d27b47c884626
trngnneee <dtn06052005@gmail.com>
2025-04-14 19:53:44 +0700

    Tạo slug dùng Mongoose-Slug-Updater

--- models/category.model.js
+++ models/category.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+var slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
 const schema = new mongoose.Schema(
   {
@@ -10,7 +12,11 @@ const schema = new mongoose.Schema(
     description: String,
     createdBy: String,
     updatedBy: String,
-    slug: String,
+    slug: {
+      type: String,
+      slug: "name", // Dựa vào name: String để tạo ra slug
+      unique: true
+    },
     deleted: {
       type: Boolean,
       default: false
