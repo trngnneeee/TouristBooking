b187eb5d1219a0c6cf1449640ea0bc9c8ffe92c1
trngnneee <dtn06052005@gmail.com>
2025-05-07 20:48:28 +0700

    Hiển thị data ra section 2

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,23 @@
-module.exports.home = (req, res) => {
+const Tours = require("../../models/tour.model")
+const moment = require("moment");
+
+module.exports.home = async (req, res) => {
+    // Section 2
+    const tourList = await Tours.find({
+        deleted: false
+    })
+
+    for (const item of tourList)
+    {
+        if (item.departureDate)
+        {
+            const departureDate = moment(item.departureDate).format("DD/MM/YYYY");
+            item.departureDateFormat = departureDate;
+        }
+    }
+    
     res.render("client/pages/home.pug", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        tourList: tourList
     })
 }
