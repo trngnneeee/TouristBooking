34452b66227ea5081f7c916e95b1fc99d92c83a1
trngnneee <dtn06052005@gmail.com>
2025-05-21 22:31:38 +0700

    Dashboard view

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,30 @@
-module.exports.dashboard = (req, res) => {
+const AccountAdmin = require("../../models/account-admin.model")
+const Orders = require("../../models/order.model")
+
+module.exports.dashboard = async (req, res) => {
+  const overView = {
+    userNum: 0,
+    orderNum: 0,
+    totalPrice: 0
+  }
+  
+  overView.userNum = await AccountAdmin.countDocuments({
+    deleted: false,
+    status: "active"
+  })
+
+  const orderList = await Orders.find({
+    deleted: false
+  });
+  overView.orderNum = orderList.length;
+
+  for (const item of orderList)
+  {
+    overView.totalPrice += item.total;
+  }
+  
   res.render("admin/pages/dashboard.pug", {
-    pageTitle: "Tổng quan"
+    pageTitle: "Tổng quan",
+    overView: overView
   })
 }
