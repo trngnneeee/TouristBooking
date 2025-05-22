48709ba98f2573978c0d3a2fa739d57c9b04b11b
trngnneee <dtn06052005@gmail.com>
2025-05-22 12:41:15 +0700

    Đổ data ra trang Dashboard

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,7 @@
 const AccountAdmin = require("../../models/account-admin.model")
 const Orders = require("../../models/order.model")
+const moment = require("moment");
+const variableConfig = require("../../config/variable.config");
 
 module.exports.dashboard = async (req, res) => {
   const overView = {
@@ -7,7 +9,7 @@ module.exports.dashboard = async (req, res) => {
     orderNum: 0,
     totalPrice: 0
   }
-  
+
   overView.userNum = await AccountAdmin.countDocuments({
     deleted: false,
     status: "active"
@@ -18,13 +20,27 @@ module.exports.dashboard = async (req, res) => {
   });
   overView.orderNum = orderList.length;
 
-  for (const item of orderList)
-  {
+  for (const item of orderList) {
     overView.totalPrice += item.total;
   }
-  
+
+  const newOrderList = await Orders.find({
+    deleted: false
+  })
+    .limit(5)
+
+  for (const order of newOrderList) {
+    order.paymentMethodName = variableConfig.paymentMethod.find(item => item.value == order.paymentMethod).label;
+    order.paymentStatusName = variableConfig.paymentStatus.find(item => item.value == order.paymentStatus).label;
+    order.statusName = variableConfig.orderStatus.find(item => item.value == order.status).label;
+
+    order.createdAtTimeFormat = moment(order.createdAt).format("HH:mm");
+    order.createdAtDateFormat = moment(order.createdAt).format("DD/MM/YYYY");
+  }
+
   res.render("admin/pages/dashboard.pug", {
     pageTitle: "Tổng quan",
-    overView: overView
+    overView: overView,
+    newOrderList: newOrderList
   })
 }
